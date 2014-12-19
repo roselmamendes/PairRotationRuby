5dc61498831e9820918f7835ad6f32598315085b
Roselma Mendes <rmendes@thoughtworks.com>
2014-12-19 17:59:11 -0300

    Added test for check if just allow create a pair if a name project is setting. Also, refactoring in rotation.rb excluding the magic numbers.

--- spec/rotation_spec.rb
+++ spec/rotation_spec.rb
@@ -1,36 +1,57 @@
 require 'spec_helper'
 
 describe Rotation do
+
+	def returnAMember(name)
+		Member.new name
+	end
+
+	before(:each) do
+		@rotation = Rotation.new
+		@rotation.project = "teste"
+	end
+
 	describe 'make a rotation' do
 		it 'should add a pair' do
-			expectedMember1 = Member.new "Matheus"
-			expectedMember2 = Member.new "Thainan"
+			expectedMember1 = returnAMember "Matheus"
+			expectedMember2 = returnAMember "Thainan"
 			pairs = [Pair.new(expectedMember1,expectedMember2)]
-			rotation = Rotation.new
-			rotation.createAPair expectedMember1,expectedMember2
+			
+			@rotation.createAPair expectedMember1,expectedMember2
 
-			expect(expectedMember1).to eq(rotation.pairs[0].member1)
-			expect(expectedMember2).to eq(rotation.pairs[0].member2)
+			expect(expectedMember1).to eq(@rotation.pairs[0].member1)
+			expect(expectedMember2).to eq(@rotation.pairs[0].member2)
 		end
 
 		it 'should add two pairs' do
-			expectedMember1 = Member.new "Matheus"
-			expectedMember2 = Member.new "Thainan"
-			expectedMember3 = Member.new "Edla"
-			expectedMember4 = Member.new "Naan"
+			expectedMember1 = returnAMember "Matheus"
+			expectedMember2 = returnAMember "Thainan"
+			expectedMember3 = returnAMember "Edla"
+			expectedMember4 = returnAMember "Naan"
 
 			pairs = [Pair.new(expectedMember1,expectedMember2),
 				Pair.new(expectedMember3,expectedMember4)]
 
-			rotation = Rotation.new
+			@rotation.createAPair(expectedMember1,expectedMember2)
+			@rotation.createAPair(expectedMember3,expectedMember4)
 
-			rotation.createAPair(expectedMember1,expectedMember2)
-			rotation.createAPair(expectedMember3,expectedMember4)
-
-			expect(expectedMember1).to eq(rotation.pairs[0].member1)
-			expect(expectedMember2).to eq(rotation.pairs[0].member2)
-			expect(expectedMember3).to eq(rotation.pairs[1].member1)
-			expect(expectedMember4).to eq(rotation.pairs[1].member2)
+			expect(expectedMember1).to eq(@rotation.pairs[0].member1)
+			expect(expectedMember2).to eq(@rotation.pairs[0].member2)
+			expect(expectedMember3).to eq(@rotation.pairs[1].member1)
+			expect(expectedMember4).to eq(@rotation.pairs[1].member2)
 		end
+
+		it 'should allow to add a member just when the project name is set' do
+			expectedMember1 = returnAMember "Matheus"
+			expectedMember2 = returnAMember "Thainan"
+			localRotation = Rotation.new
+
+			pairs = [Pair.new(expectedMember1,expectedMember2)]
+
+			result = localRotation.createAPair(expectedMember1,expectedMember2)
+
+			expect(result).to eq(-1)
+		end
+
 	end 
 end
--- src/rotation.rb
+++ src/rotation.rb
@@ -1,13 +1,21 @@
 require_relative '../src/pair'
 
+CAN_NOT_PROCEED = -1
+SUCESS = 1
+
 class Rotation
-	attr_accessor :pairs
+	attr_accessor :pairs,:project
 
 	def initialize 
 		@pairs = Array.new
+		@project = ""
 	end
 
 	def createAPair member1,member2
-		@pairs.push(Pair.new member1,member2)
+		if(!@project.empty?)
+			@pairs.push(Pair.new member1,member2)
+			return SUCESS
+		end
+		CAN_NOT_PROCEED
 	end
 end
